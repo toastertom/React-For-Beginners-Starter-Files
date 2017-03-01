2133731833e0ba697184a75d0b6ddb0ba67262af
ToasterTom <tate.thomas5595@gmail.com>
2017-03-01 16:28:53 -0700

    Added routing

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 // Sometimes people use import React-DOM from 'react-dom'; but we only need the render method so we just use the code below.
 import { render } from 'react-dom';
+import {BrowserRouter, Match, Miss} from 'react-router';
 // You can load your styles the normal way in the html document but this way lets webpack do a hot reload when you make a change.
 import './css/style.css';
 
 //Components
-import StorePicker from './components/StorePicker';
 import App from './components/App';
-import Header from './components/Header';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
+// Stateless Functional Component
+const Root = () => {
+  return(
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker}/>
+        <Match exactly pattern="/store/:storeId" component={App}/>
+        <Miss component={NotFound}/>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-render(<App/>, document.querySelector('#main'));
+render(<Root/>, document.querySelector('#main'));
