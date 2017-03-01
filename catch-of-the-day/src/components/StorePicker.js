a30dd24dbb4b1a9b82708a02084353ba64d30289
ToasterTom <tate.thomas5595@gmail.com>
2017-03-01 16:58:06 -0700

    Working With React Events

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,11 +3,23 @@ import { getFunName } from '../helpers.js';
 
 // Component (Everything is done in ES6)
 class StorePicker extends React.Component {
+
+  // this. only refers to StorePicker automatically on the render Method.
+  //To get it to work on custome methods you have to make this constructor method below and bind it. 
+  constructor(){
+    super();
+    this.goToStore =this.goToStore.bind(this);
+  }
+
+  goToStore(e){
+    e.preventDefault();
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={ getFunName()} />
+        <input type="text" required placeholder="Store Name" defaultValue={ getFunName()} ref={(input) => {this.storeInput = input}} />
         <button type="submit">Visit Store</button>
       </form>
     );
